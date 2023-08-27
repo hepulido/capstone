bc92897b54a08537078289defa582b482dce2e6b
hector <hpulido1021@gmail.com>
2023-08-27 18:28:02 -0400

    Feature: install react-router and create routes

--- capstone-eccomerce/src/App.js
+++ capstone-eccomerce/src/App.js
@@ -1,10 +1,27 @@
 import { Home } from './pages/Home';
+import { Route, Routes } from "react-router-dom";
 import './App.css';
+import { LogIn } from './pages/LogIn';
 
 function App() {
   return (
     <div className="App">
-     <Home/>
+     <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <Home/>
+            }
+          />
+             <Route
+            exact
+            path="/logIn"
+            element={
+              <LogIn/>
+            }
+          />
+      </Routes>
     </div>
   );
 }
--- capstone-eccomerce/src/pages/LogIn.jsx
+++ capstone-eccomerce/src/pages/LogIn.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const LogIn = () => {
   return (
-    <div>LogIn</div>
+    <div>Hello world from LogIn</div>
   )
 }
