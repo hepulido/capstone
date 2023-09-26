535ce2d7b620c53cc128d853d2ff21e7ad08ff89
hector <hpulido1021@gmail.com>
2023-09-25 20:48:04 -0400

    Feature: fixed css of about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,30 +1,49 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Navbar } from "../components/Navbar";
+import styled from "styled-components";
+import { mobile } from "../responsive";
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100vh;
+`;
+const AboutCard = styled.div`
+  background-color: white;
+  text-align: center;
+  font-size: 18px;
+  font-weight: 500;
+  border-radius: 10px;
+  padding: 16px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
+  max-width: 1000px;
+
+  ${mobile({ width: "100vw" })}
+`;
 export function About() {
   return (
-    <div>
+    <>
       <Navbar />
-      <div className="container py-5 my-5">
-        <div className="row">
-          <div className="col-md-6">
-            <h1 className="text-primary fw-bold mb-4">About Us</h1>
-            <p className="lead mb-4">
-              Our work is above all joyful. We do what we love, and we love what
-              we do. Our pieces help mark new chapters, close old ones, remind
-              us of our roots, and launch new lifetimes..
-              <br />
-              We are a store located in Miami Florida, we have many years of
-              experience in the market, this company was created based on a
-              Fullsctack Academy school project and ended up being a reality.
-            </p>
-            <Link to="/" style={{ textDecoration: "none" }}>
-              Contact Us
-            </Link>
-          </div>
-        </div>
-      </div>
-    </div>
+      <Container>
+        <AboutCard>
+          <h1>About Us</h1>
+          <p>
+            Our work is above all joyful. We do what we love, and we love what
+            we do. Our pieces help mark new chapters, close old ones, remind us
+            of our roots, and launch new lifetimes..
+            <br />
+            <br />
+            We are a store located in Miami Florida, we have many years of
+            experience in the market, this company was created based on a
+            Fullsctack Academy school project and ended up being a reality.
+          </p>
+          <Link to="/" style={{ textDecoration: "none" }}>
+            Contact Us
+          </Link>
+        </AboutCard>
+      </Container>
+    </>
   );
 }
